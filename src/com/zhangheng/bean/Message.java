61bfbf277130a9634c57307294ee39b9e7c76f9a
ZhangHeng0805 <3052847663@qq.com>
2022-04-08 16:00:59 +0800

    功能优化

--- src/com/zhangheng/bean/Message.java
+++ src/com/zhangheng/bean/Message.java
@@ -66,7 +66,7 @@ public class Message {
                 ", code=" + code +
                 ", title='" + title + '\'' +
                 ", message='" + message + '\'' +
-                ", obj=" + obj +
+                ", obj=" + obj.toString() +
                 '}';
     }
 
